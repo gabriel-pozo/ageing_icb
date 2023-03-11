a63be63ee1c5dec5d65fc177a3c51521f34802e5
gabriel-pozo <91978316+gabriel-pozo@users.noreply.github.com>
2023-03-11 12:08:07 -0300

    Update icb_scvi_integration.py

--- icb_scvi_integration.py
+++ icb_scvi_integration.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[]:
 
 
 #Load the libraries
@@ -27,14 +27,14 @@ import anndata
 import hotspot
 
 
-# In[2]:
+# In[]:
 
 
 # load sparse matrix:
 X = io.mmread("D:Scanpy/new_york/counts.mtx")
 
 
-# In[3]:
+# In[]:
 
 
 # create anndata object
@@ -43,14 +43,14 @@ ny = anndata.AnnData(
 )
 
 
-# In[4]:
+# In[]:
 
 
 # load cell metadata:
 cell_meta = pd.read_csv("D:Scanpy/new_york/metadata.csv")
 
 
-# In[5]:
+# In[]:
 
 
 # load gene names:
@@ -58,7 +58,7 @@ with open("D:Scanpy/new_york/genes.csv", 'r') as f:
  gene_names = f.read().splitlines()
 
 
-# In[6]:
+# In[]:
 
 
 # set anndata observations and index obs by barcodes, var by gene names
@@ -67,20 +67,20 @@ ny.obs.index = ny.obs['barcode']
 ny.var.index = gene_names
 
 
-# In[7]:
+# In[]:
 
 
 del(cell_meta, gene_names, X)
 
 
-# In[8]:
+# In[]:
 
 
 # load sparse matrix:
 X = io.mmread("D:Scanpy/md_anderson/counts.mtx")
 
 
-# In[9]:
+# In[]:
 
 
 # create anndata object
@@ -89,14 +89,14 @@ md = anndata.AnnData(
 )
 
 
-# In[10]:
+# In[]:
 
 
 # load cell metadata:
 cell_meta = pd.read_csv("D:Scanpy/md_anderson/metadata.csv")
 
 
-# In[11]:
+# In[]:
 
 
 # load gene names:
@@ -104,7 +104,7 @@ with open("D:Scanpy/md_anderson/genes.csv", 'r') as f:
  gene_names = f.read().splitlines()
 
 
-# In[12]:
+# In[]:
 
 
 # set anndata observations and index obs by barcodes, var by gene names
@@ -113,32 +113,32 @@ md.obs.index = md.obs['barcode']
 md.var.index = gene_names
 
 
-# In[13]:
+# In[]:
 
 
 del(cell_meta, gene_names, X)
 
 
-# In[14]:
+# In[]:
 
 
 adata = md.concatenate(ny)
 
 
-# In[15]:
+# In[]:
 
 
 del(md, ny)
 
 
-# In[16]:
+# In[]:
 
 
 adata.obs.index=adata.obs['barcode']
 adata.obs
 
 
-# In[17]:
+# In[]:
 
 
 def categorise(row):  
@@ -148,19 +148,19 @@ def categorise(row):
         return 'Non-elderly'
 
 
-# In[18]:
+# In[]:
 
 
 adata.obs['status'] = adata.obs.apply(lambda row: categorise(row), axis=1)
 
 
-# In[19]:
+# In[]:
 
 
 adata.layers['counts'] = adata.X.copy()
 
 
-# In[20]:
+# In[]:
 
 
 sc.pp.normalize_total(adata, target_sum=1e4)
@@ -168,92 +168,92 @@ sc.pp.log1p(adata)
 adata.raw = adata
 
 
-# In[21]:
+# In[]:
 
 
 condition_key = "sample_id"
 
 
-# In[22]:
+# In[]:
 
 
 sc.pp.highly_variable_genes(adata, flavor='seurat', n_top_genes=3000, layer='counts', subset=True, batch_key=condition_key)
 
 
-# In[23]:
+# In[]:
 
 
 scvi.model.SCVI.setup_anndata(adata, layer = "counts", batch_key = condition_key,
  continuous_covariate_keys=['nCount_RNA', 'percent.mt'])
 
 
-# In[24]:
+# In[]:
 
 
 model = scvi.model.SCVI(adata, n_layers=2, n_latent=30)
 
 
-# In[25]:
+# In[]:
 
 
 model.train(max_epochs=100)
 
 
-# In[26]:
+# In[]:
 
 
 adata.obsm['X_scVI'] = model.get_latent_representation()
 
 
-# In[27]:
+# In[]:
 
 
 sc.pp.neighbors(adata, use_rep = 'X_scVI')
 
 
-# In[28]:
+# In[]:
 
 
 sc.tl.umap(adata)
 
 
-# In[29]:
+# In[]:
 
 
 sc.pl.umap(adata, color = 'sample_id', frameon=False)
 
 
-# In[31]:
+# In[]:
 
 
 sc.pl.umap(adata, color = ['status', 'ICB_response', 'Stage', 'patient_id'], frameon=False, ncols=2)
 
 
-# In[33]:
+# In[]:
 
 
 sc.pl.umap(adata, color = ['CD4', 'CD8A', 'IL7R', 'CCR7', 'SELL', 'IL2RA', 'FOXP3', 'NKG7'], cmap='viridis', ncols=4, frameon=False)
 
 
-# In[73]:
+# In[]:
 
 
 sc.tl.leiden(adata, resolution=3)
 
 
-# In[75]:
+# In[]:
 
 
 sc.pl.umap(adata, color='leiden', frameon=False, legend_loc='on data', legend_fontoutline=1, legend_fontsize = 8)
 
 
-# In[76]:
+# In[]:
 
 
 sc.tl.rank_genes_groups(adata, 'leiden', method='wilcoxon')
 
 
-# In[77]:
+# In[]:
 
 
 markers = sc.get.rank_genes_groups_df(adata, None)
@@ -261,97 +261,25 @@ markers = markers[(markers.pvals_adj < 0.05) & (markers.logfoldchanges > .5)]
 markers
 
 
-# In[78]:
+# In[]:
 
 
 markers.to_csv("D:Scanpy/marcadores_elderly_419k.csv")
 
 
-# In[100]:
-
-
-#markers[markers.group=='18'].head(10)
-
-
-# In[99]:
-
-
-#markers[markers.group=='19'].head(10)
-
-
-# In[98]:
-
-
-#markers[markers.group=='25'].head(10)
-
-
-# In[97]:
-
-
-#markers[markers.group=='28'].head(10)
-
-
-# In[96]:
-
-
-#markers[markers.group=='1'].head(10)
-
-
-# In[95]:
-
-
-#markers[markers.group=='22'].head(10)
-
-
-# In[94]:
-
-
-#markers[markers.group=='16'].head(10)
-
-
-# In[93]:
-
-
-#markers[markers.group=='32'].head(10)
-
-
-# In[86]:
-
-
-markers[markers.names=='CD4']
-
-
-# In[87]:
-
-
-markers[markers.names=='FOXP3']
-
-
-# In[88]:
-
-
-markers[markers.names=='CD8A']
-
-
-# In[89]:
-
-
-markers[markers.names=='CD8B']
-
-
-# In[91]:
+# In[]:
 
 
 sc.pl.umap(adata, color = ['leiden', 'CD4', 'CD8A'], frameon=False, groups=('18', '19', '1', '16', '38'), cmap='viridis', ncols=3)
 
 
-# In[106]:
+# In[]:
 
 
 sc.pl.umap(adata, color = ['leiden', 'CD4', 'CD8A'], frameon=False, groups=('12'), cmap='viridis', ncols=3)
 
 
-# In[107]:
+# In[]:
 
 
 cell_type = {"0":"CD4-Treg",
@@ -396,33 +324,33 @@ cell_type = {"0":"CD4-Treg",
 "39":"CD4"}
 
 
-# In[108]:
+# In[]:
 
 
 adata.obs['cell_type'] = adata.obs.leiden.map(cell_type)
 
 
-# In[109]:
+# In[]:
 
 
 sc.set_figure_params(dpi=200)
 sc.pl.umap(adata, color = ['cell_type', 'status', 'Stage', 'ICB_response'], frameon=False, ncols=2)
 
 
-# In[56]:
+# In[]:
 
 
 adata.write("D:Scanpy/artigo_elderly_ICB.h5ad")
 model.save("D:Scanpy/elderly_ICB_model.model")
 
 
-# In[113]:
+# In[]:
 
 
 adata.obs.groupby(['patient_id']).count()
 
 
-# In[114]:
+# In[]:
 
 
 num_tot_cells = adata.obs.groupby(['patient_id']).count()
@@ -430,7 +358,7 @@ num_tot_cells = dict(zip(num_tot_cells.index, num_tot_cells.leiden))
 num_tot_cells
 
 
-# In[118]:
+# In[]:
 
 
 cell_type_counts = adata.obs.groupby(['patient_id', 'status', 'cell_type']).count()
@@ -439,7 +367,7 @@ cell_type_counts = cell_type_counts[cell_type_counts.columns[0:5]]
 cell_type_counts
 
 
-# In[119]:
+# In[]:
 
 
 cell_type_counts['total_cells'] = cell_type_counts.patient_id.map(num_tot_cells).astype(int)
@@ -447,7 +375,7 @@ cell_type_counts['frequency'] = cell_type_counts.nCount_RNA / cell_type_counts.t
 cell_type_counts
 
 
-# In[124]:
+# In[]:
 
 
 sc.set_figure_params(dpi=100)
@@ -459,7 +387,7 @@ plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0)
 plt.show()
 
 
-# In[2]:
+# In[]:
 
 
 cell_meta1 = pd.read_csv("D:Scanpy/cell_meta1_elderly.csv", index_col=0)
@@ -469,69 +397,38 @@ df = pd.concat([cell_meta1, cell_meta2, cell_meta3])
 df
 
 
-# In[3]:
-
-
-del(df['barcode.1'])
-
-
-# In[4]:
-
-
-df
-
-
-# In[5]:
-
-
-adata = sc.read("D:Scanpy/artigo_elderly_ICB.h5ad")
-adata.obs
-
-
-# In[13]:
-
-
-adata[adata.obs.cell_type=='MAIT']
-
-
-# In[12]:
+# In[]:
 
 
 df.groupby(['celltype_refined']).count()
 
 
-# In[14]:
+# In[]:
 
 
 adata.obs['celltype_refined'] = df.celltype_refined
 
 
-# In[16]:
+# In[]:
 
 
 sc.set_figure_params(dpi=200)
 sc.pl.umap(adata, color = ['cell_type', 'celltype_refined'], ncols=2, frameon=False)
 
 
-# In[17]:
+# In[]:
 
 
 adata.obs.to_csv("D:Scanpy/metadata_elderly_final.csv")
 
 
-# In[18]:
-
-
-del(df, cell_meta1, cell_meta2, cell_meta3)
-
-
-# In[19]:
+# In[]:
 
 
 adata.obs.groupby(['patient_id']).count()
 
 
-# In[16]:
+# In[]:
 
 
 num_tot_cells = adata.obs.groupby(['patient_id']).count()
@@ -539,7 +436,7 @@ num_tot_cells = dict(zip(num_tot_cells.index, num_tot_cells.leiden))
 num_tot_cells
 
 
-# In[21]:
+# In[]:
 
 
 cell_type_counts = adata.obs.groupby(['patient_id', 'status', 'celltype_refined']).count()
@@ -548,7 +445,7 @@ cell_type_counts = cell_type_counts[cell_type_counts.columns[0:5]]
 cell_type_counts
 
 
-# In[22]:
+# In[]:
 
 
 cell_type_counts['total_cells'] = cell_type_counts.patient_id.map(num_tot_cells).astype(int)
@@ -556,7 +453,7 @@ cell_type_counts['frequency'] = cell_type_counts.nCount_RNA / cell_type_counts.t
 cell_type_counts
 
 
-# In[23]:
+# In[]:
 
 
 import matplotlib.pyplot as plt
@@ -567,7 +464,7 @@ plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0)
 plt.show()
 
 
-# In[29]:
+# In[]:
 
 
 cell_type_counts = adata.obs.groupby(['patient_id', 'ICB_response', 'celltype_refined']).count()
@@ -576,7 +473,7 @@ cell_type_counts = cell_type_counts[cell_type_counts.columns[0:5]]
 cell_type_counts
 
 
-# In[30]:
+# In[]:
 
 
 cell_type_counts['total_cells'] = cell_type_counts.patient_id.map(num_tot_cells).astype(int)
@@ -584,7 +481,7 @@ cell_type_counts['frequency'] = cell_type_counts.nCount_RNA / cell_type_counts.t
 cell_type_counts
 
 
-# In[31]:
+# In[]:
 
 
 import matplotlib.pyplot as plt
@@ -595,19 +492,13 @@ plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0)
 plt.show()
 
 
-# In[2]:
-
-
-adata = sc.read("D:Scanpy/artigo_elderly_ICB_final.h5ad")
-
-
-# In[3]:
+# In[]:
 
 
 ax = sc.pl.correlation_matrix(adata, 'celltype_refined')
 
 
-# In[3]:
+# In[]:
 
 
 sc.set_figure_params(dpi=300)
@@ -616,7 +507,7 @@ sc.pl.umap(adata, color = ['cell_type', 'status', 'ICB_response', 'patient_id'],
           show=False, size=1)
 
 
-# In[4]:
+# In[]:
 
 
 num_total_cells = adata.obs.groupby(['patient_id']).count()
@@ -624,7 +515,7 @@ num_total_cells = dict(zip(num_total_cells.index, num_total_cells.leiden))
 num_total_cells
 
 
-# In[10]:
+# In[]:
 
 
 cell_type_counts = adata.obs.groupby(['patient_id', 'status', 'celltype_refined']).count()
@@ -633,7 +524,7 @@ cell_type_counts = cell_type_counts[cell_type_counts.columns[0:5]]
 cell_type_counts
 
 
-# In[11]:
+# In[]:
 
 
 cell_type_counts['total_cells'] = cell_type_counts.patient_id.map(num_total_cells).astype(int)
@@ -641,7 +532,7 @@ cell_type_counts['frequency'] = cell_type_counts.nCount_RNA / cell_type_counts.t
 cell_type_counts
 
 
-# In[12]:
+# In[]:
 
 
 sc.set_figure_params(dpi=300)
@@ -652,7 +543,7 @@ plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0)
 plt.show()
 
 
-# In[3]:
+# In[]:
 
 
 df = (adata.obs
@@ -664,7 +555,7 @@ df = (adata.obs
 df
 
 
-# In[6]:
+# In[]:
 
 
 colors = ['#2ca02c', '#1f77b4', '#ff7f0e', '#d62728']
@@ -676,7 +567,7 @@ plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0)
 plt.show()
 
 
-# In[22]:
+# In[]:
 
 
 sc.set_figure_params(dpi=300)
@@ -685,13 +576,7 @@ sc.pl.umap(adata, color = ['CD8A', 'CD4', 'FOXP3', 'TRBV18'], frameon=False, nco
           show=False, size=1, cmap='viridis')
 
 
-# In[2]:
-
-
-adata = sc.read("D:Scanpy/artigo_elderly_ICB_final.h5ad")
-
-
-# In[3]:
+# In[]:
 
 
 sc.set_figure_params(dpi=200)
@@ -703,4 +588,3 @@ marker_genes_dict = {
     'MAIT': ['TRAV13-1', 'KLRB1', 'CCR6', 'IL4I1'],
 }
 sc.pl.dotplot(adata, marker_genes_dict, 'cell_type', dendrogram=True)
-
